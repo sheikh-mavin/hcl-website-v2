d81d2984152a674641b929d2630ff94a1cfc2fc3
sheikh_mavin <sheikh.mavin@tothenew.com>
2022-08-16 12:34:10 +0530

    minor fix

--- js/fullpage.js
+++ js/fullpage.js
@@ -3,8 +3,8 @@ var $fh = $("#f-h");
 var $eh = $("#e-h");
 var $qh = $("#q-h");
 
-var $leftArrow = $(".left-arrow");
-var $rightArrow = $(".right-arrow");
+var $leftArrow = $(".swiper-left-arrow");
+var $rightArrow = $(".swiper-right-arrow");
 var $customerTestimonials = $("#customers-testimonials");
 var $featureCardOne = $("#feature-card-one");
 var $featureCardTwo = $("#feature-card-two");
@@ -91,23 +91,23 @@ $("#fullpage").fullpage({
         .addClass("animate__animated animate__fadeInRightBig ")
         .css("animation-delay", "1s");
       $leftArrow
-        .addClass("animate__animated animate__fadeInLeftBig animate__delay-1s")
+        .addClass("animate__animated animate__fadeInLeftBig animate__delay-2s")
         .css("animation-delay", ".6s");
       $rightArrow
-        .addClass("animate__animated animate__fadeInRightBig animate__delay-1s")
+        .addClass("animate__animated animate__fadeInRightBig animate__delay-2s")
         .css("animation-delay", ".6s");
       // $customerTestimonials.addClass(
       //   "animate__animated animate__fadeInUp"
       // );
 
       if (count == 0) {
-        setTimeout(function () {
-          owl.trigger("next.owl.carousel", [400]);
-          owl.trigger("next.owl.carousel", [800]);
-          owl.trigger("next.owl.carousel", [1000]);
-          owl.trigger("next.owl.carousel", [1200]);
-          // owl.trigger("next.owl.carousel", [2400]);
-        }, 500);
+        // setTimeout(function () {
+        //   owl.trigger("next.owl.carousel", [400]);
+        //   owl.trigger("next.owl.carousel", [800]);
+        //   owl.trigger("next.owl.carousel", [1000]);
+        //   owl.trigger("next.owl.carousel", [1200]);
+        //   // owl.trigger("next.owl.carousel", [2400]);
+        // }, 500);
 
         $('.swiper-slide:nth-child(1n)').addClass("transition-duration-1n");
         $('.swiper-slide:nth-child(2n)').addClass("transition-duration-2n");
